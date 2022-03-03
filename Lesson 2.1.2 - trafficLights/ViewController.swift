61d94d4f6bdb4462a79ba81b35e777006f40e733
AlexVerkhov <alexverkhov@gmail.com>
2022-03-03 12:29:55 +0700

    Initial Commit

---
+++ Lesson 2.1.2 - trafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.1.2 - trafficLights
+//
+//  Created by Алексей Верховых on 03.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
